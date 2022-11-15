7c17574c74456ee1331d75bfa3a8e9ce182a0aa9
CodeWaffl3 <0218762@up.edu.mx>
2022-11-14 20:33:16 -0600

    practica 4

--- practicas/practica3/practicas/src/App.vue
+++ practicas/practica3/practicas/src/App.vue
@@ -7,12 +7,25 @@
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  created() {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, "0218762@up.edu.mx","password").then(userCredential =>{
+      const user = userCredential.user;
+      if(user == null) {
+        console.log("NO AUNTENTICADO");
+      }
+      else {
+        console.log("Auntenticado correctamente", user);
+      }
+    })
   }
 });
 </script>
--- practicas/practica3/practicas/src/main.js
+++ practicas/practica3/practicas/src/main.js
@@ -20,12 +20,34 @@ import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
+/*FIREBASE */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeTt_PYWq2bu7z-R02RdHtlE65LLWVn9A",
+  authDomain: "ionicdev-8e5a6.firebaseapp.com",
+  projectId: "ionicdev-8e5a6",
+  storageBucket: "ionicdev-8e5a6.appspot.com",
+  messagingSenderId: "705914372776",
+  appId: "1:705914372776:web:2611893480a43698aeb89d",
+  measurementId: "G-WJZBZHL8FN"
+};
+
+// Initialize Firebase
+const appFireBase = initializeApp(firebaseConfig);
+
 /* Theme variables */
 import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appFireBase);
   
 router.isReady().then(() => {
   app.mount('#app');
